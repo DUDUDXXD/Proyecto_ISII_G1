db2d16136d2f7566faf1b29739229b36597eb3ae
monronma <monronma@alumni.uv.es>
2024-10-22 12:16:14 +0200

    Prueba 1

--- JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package proyectoisii;
+package javaapplication3;
 
 /**
  *
  * @author mmati
  */
-public class ProyectoISII {
+public class JavaApplication3 {
 
     /**
      * @param args the command line arguments
